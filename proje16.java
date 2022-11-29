6729654fafaf83bff0c2e1a023e5773d1b9174d8
nurbeyzanur <117708453+nurbeyzanur@users.noreply.github.com>
2022-11-29 17:35:04 +0300

    Update proje16.java
    Klavyeden girilen x sayısına göre f(x)=x*2-5x+3  fonksiyonun signum fonksiyonuna göre işaretini hesaplayan program

--- proje16.java
+++ proje16.java
@@ -1,21 +1,25 @@
-
 package algoritmaprojeleri; 
-               //klavyeden girilen x sayısına göre x*2-5x+3 hesaplayan program
+    //klavyeden girilen x sayısına göre x*2-5x+3 hesaplayan program
 import java.util.Scanner;
 
-
-            public class proje16 {
-                public static void main(String[] args) {
-                    Scanner scan=new Scanner(System.in);
-                   int x,cevap;
+      public class proje16 {
+        public static void main(String[] args) {
+             Scanner scan = new Scanner(System.in);
+             int x,cevap;
                    
-                    System.out.println("Lütfen bir x değeri giriniz");
-                    x=scan.nextInt();
+             System.out.println("Lütfen bir x değeri giriniz");
+              x = scan.nextInt();                    
+                cevap = x*x-5*x+3;                    
+               
+                if (cevap<0){
+                    System.out.println("-1");
+                  }
+                else if(cevap>0){
+                    System.out.println("1");                    
+                  }
+                else 
+                    System.out.println("0");
                     
-                    cevap=x*x-5*x+3;
-                    
-                     System.out.println("İşlemin sonucu:"+cevap);
-                    
-                   
-                }
+        }
+        
 }
