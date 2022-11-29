b4e927b7b29619971e55624c5c34848a5917456f
nurbeyzanur <117708453+nurbeyzanur@users.noreply.github.com>
2022-11-29 21:29:57 +0300

    Add files via upload
    Klavyeden girilen pozitif bir a tamsayısının tam bölenlerini hesaplayıp listeleyen program

---
+++ proje19.java
@@ -0,0 +1,22 @@
+//klavyeden girilen poziif bi a tamsayısının tam bölenlerini hesaplayıp listeleyen program
+
+package algoritmaprojeleri;
+
+import java.util.Scanner;
+
+public class proje19 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+         int n;
+         
+        System.out.println("Lütfen bir sayı giriniz:");
+        n = scan.nextInt();
+        
+        for(int i=1; i<=n ; i++){
+        if (n%i==0) {
+            System.out.print(" "+i);            
+           }        
+        }
+    }
+    
+}
